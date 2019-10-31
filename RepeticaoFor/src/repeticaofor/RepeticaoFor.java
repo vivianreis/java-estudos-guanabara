6a2e8427221508b122f665f42f53b18ca22b2c04
Vivian Barbosa Reis <srtvivian@hotmail.com>
2019-10-31 19:18:54 -0300

    estrutura de repeticao for

---
+++ RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repeticaofor;
+
+/**
+ *
+ * @author VivianReis
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //for (int cc=0; cc<=3; cc++){
+        //    System.out.println("Cambalhota");
+        //}
+        
+        for(int cc=1; cc<=100; cc += + 10){
+            System.out.println("Cambalhota " + cc);
+        }
+    }
+    
+}
