6b0c49f68d7692afc4e36add00885d1044f50ce1
Vivian Barbosa Reis <srtvivian@hotmail.com>
2019-11-01 14:52:20 -0300

    vetores

---
+++ Vetor02/src/vetor02/Vetor02.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vetor02;
+
+/**
+ *
+ * @author VivianReis
+ */
+public class Vetor02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String mes[] = {"Jan", "Fev", "Mar", "Abr","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez"};
+        int tot[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        
+        for(int cc=0; cc<=mes.length; cc++){
+            System.out.println("O mês de " + mes[cc] + " tem " + tot[cc] + " dias");
+        }
+    }  
+}
